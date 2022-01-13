1497aecf70b77b4cda10f8d0f64bc8947f2f1b5a
stepsword <40430825+stepsword@users.noreply.github.com>
2022-01-12 19:34:53 -0500

    1.17

---
+++ src/main/java/stepsword/potionidpacketfixer/mixin/MixinClientPacketListener.java
@@ -0,0 +1,29 @@
+package stepsword.potionidpacketfixer.mixin;
+
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
+import net.minecraft.world.effect.MobEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import stepsword.potionidpacketfixer.interfaces.ClientboundUpdateMobEffectPacketGetter;
+
+@Mixin(ClientPacketListener.class)
+public class MixinClientPacketListener {
+    @Redirect(
+        // the method this function is called in
+        method = "handleUpdateMobEffect(Lnet/minecraft/network/protocol/game/ClientboundUpdateMobEffectPacket;)V",
+        // target the invocation of System.out.println
+        at = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/world/effect/MobEffect;byId(I)Lnet/minecraft/world/effect/MobEffect;"
+        )
+    )
+    private MobEffect getEffect(int z, ClientboundUpdateMobEffectPacket packet) {
+        if (packet instanceof ClientboundUpdateMobEffectPacketGetter)
+            return MobEffect.byId(((ClientboundUpdateMobEffectPacketGetter) packet).getIntEffectId());
+        else
+            return MobEffect.byId(z);
+    }
+
+}
